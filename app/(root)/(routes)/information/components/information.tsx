f94f299d29baa0e2730ba704cb175fa111b42523
MiketheBite <migueljcpinto@mail.com>
2024-03-09 20:19:32 +0100

    text corrections

--- app/(root)/(routes)/information/components/information.tsx
+++ app/(root)/(routes)/information/components/information.tsx
@@ -1,62 +1,63 @@
 export const Information = () => {
   return (
-    <div className="container pt-5 pb-5 space-y-4">
-      <h1 className="text-2xl font-bold text-center">Informações</h1>
-      <div className="space-y-10 p-5 ">
-        <p className="bg-primary/10 rounded-xl border-2 p-5">
-          Bem-vindo ao AI Candidato, um projeto fascinante que mergulha no mundo
-          da inteligência artificial para criar Chatbots simulando conversas de
-          candidatos políticos nas eleições legislativas de Portugal. Este
+    <div className="container pt-5 pb-5 space-y-3">
+      <h1 className="text-2xl font-bold text-center ">Informações</h1>
+      <div className="space-y-7 p-5 ">
+        <p className="bg-primary/10 rounded-xl border-2 p-5 text-sm md:text-base">
+          Bem-vindo ao AI Candidato, um projeto que procura utilizar a
+          inteligência artificial para criar Chatbots, simulando conversas de
+          candidatos políticos para as eleições legislativas de Portugal. Este
           projeto tem como objetivo primordial explorar e aprender sobre o
           desenvolvimento de Chatbots e aplicações utilizando inteligência
-          artificial, oferecendo uma experiência única e envolvente.
+          artificial, oferecendo uma experiência única.
         </p>
       </div>
 
-      <div className="p-6 space-y-4">
+      <div className="p-6 space-y-4 text-sm md:text-base">
         <p>
           É importante salientar que todos os Chatbots foram treinados com
           informações limitadas e não atualizadas. Este projeto não visa
           fornecer dados precisos ou atualizados sobre os candidatos ou as
-          eleições em questão. Assim, recomendamos que confira sempre a
-          veracidade das informações dadas pelos Chatbots e consulte os
-          Programas Políticos de cada Partido para informações precisas.
+          eleições em questão. Assim, recomendamos que confiras sempre a
+          veracidade das informações dadas pelos Chatbots e consultes os
+          Programas Políticos de cada Partido para informações mais precisas.
         </p>
         <p>
-          Por favor, tenha em mente que as informações e opiniões expressas
-          pelos Candidatos nos Chatbots podem não ser reais. Eles são
-          representações caricatas dos candidatos e seus partidos, podendo não
-          refletir suas posições reais sobre os temas abordados.
+          Por favor, tem em mente que as informações e opiniões expressas pelos
+          Candidatos nos Chatbots podem não ser reais. Eles são representações
+          fictícias dos candidatos e dos seus partidos, podendo não refletir as
+          suas posições reais sobre os temas abordados.
         </p>
         <p>
-          Este projeto não tem qualquer intenção de influenciar a opinião do
-          eleitorado. Trata-se de um exercício criativo e não deve ser
-          interpretado como um ataque a qualquer candidato ou partido político.
-          Como criador deste projeto, não me responsabilizo por qualquer uso
-          indevido das informações aqui contidas.
+          Este projeto não tem qualquer intenção de influênciar a opinião do
+          eleitorado. Trata-se de um exercício educativo e, por isso, não deve
+          ser interpretado como um ataque a qualquer candidato ou partido
+          político.
         </p>
       </div>
 
-      <div className=" p-5 space-y-3">
+      <div className="text-sm p-5 space-y-3 md:text-base">
         <div className=" bg-primary/10 rounded-xl border-2 space-y-5 p-5">
-          <p className="font-bold">Lembre-se:</p>
+          <p className="text-sm font-semibold md:text-base">Lembra-te:</p>
           <ul className="list-disc pl-4 space-y-1">
             <li>O conteúdo pode não estar atualizado.</li>
             <li>As informações dos Chatbots NÃO são reais.</li>
-            <li>Não leve nada a sério! Divirta-se!</li>
+            <li>Não leves nada a sério! Informa-te!</li>
           </ul>
 
+          <p className="text-sm font-semibold md:text-base">
+            Sobre o projecto:
+          </p>
           <p>
-            <span className="font-semibold ">Sobre o projecto:</span> foram
-            usadas várias tecnologias, incluindo Next.js, React, Typescript,
-            Tailwind, Prisma, ferramentas de criação de imagens e conversação
-            (Open AI) e Clerk para autenticação eficiente.
+            Foram usadas várias tecnologias, incluindo Next.js, React,
+            Typescript, Tailwind, Prisma, ferramentas de criação de imagens e
+            conversação (Open AI) e Clerk para autenticação eficiente.
           </p>
         </div>
 
         <div>
-          <p className="p-4 text-end">
-            Espero que se divirta com o AI Candidato!
+          <p className="p-4 text-end text-sm md:text-base">
+            Divirte-te com o AI Candidato!
           </p>
         </div>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
-import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,7 +27,6 @@ export default function RootLayout({
         <body className={cn("bg-secondary", inter.className)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
-            <Footer />
             <Toaster />
           </ThemeProvider>
           <Analytics />
